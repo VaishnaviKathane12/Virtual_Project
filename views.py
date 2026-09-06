835f095a6c54bb3488ba133cb95e0f4ad902bf83
vaishnavikathane12 <vaishnavikathane.official@gmail.com>
2026-09-06 15:06:41 +0530

    learn dynamic website

--- views.py
+++ views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h2> Hey, Welcome</h2>')
+    
+    return render(request, 'index.html')       #adding address of index.html
+
+def counter(request):
+    text = request.POST['text']
+    no_of_words = len(text.split())
+    return render(request, 'counter.html', {'number' : no_of_words})
