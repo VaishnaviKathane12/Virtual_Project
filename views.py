8d54a8bc6ada71dcaf2a6fe1cf8e27a7f024f9fd
vaishnavikathane12 <vaishnavikathane.official@gmail.com>
2026-09-06 22:32:36 +0530

    done and learn about static model and also use already made html template

--- views.py
+++ views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Feature
 # Create your views here.
 def index(request):
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = "Fast"
+    feature1.details = 'Our service is very quick'
     
-    return render(request, 'index.html')       #adding address of index.html
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = "Reliable"
+    feature2.details = 'Our service is very reliable'
+    
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = "Easy To Use"
+    feature3.details = 'Our service is easy to use'
+    
+    feature4 = Feature()
+    feature4.id = 3
+    feature4.name = "Affordable"
+    feature4.details = 'Our service is very affordable'
+    
+    return render(request, 'index.html', {'feature1': feature1, 'feature2': feature2, 'feature3': feature3, 'feature4': feature4})       #adding address of index.html
 
 def counter(request):
     text = request.POST['text']
